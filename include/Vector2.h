febaaefb7c9b2e3085c6f6321f438267361c3ed0
oxtna <rafalkajomof2709@gmail.com>
2022-03-10 21:58:24 +0100

    Add scalar multipication and division, and Normalize method to Vector2

--- include/Vector2.h
+++ include/Vector2.h
@@ -6,9 +6,14 @@ public:
 	float X;
 	float Y;
 	Vector2(float x, float y);
+	Vector2 Normalize() const;
 	Vector2 operator+(const Vector2& other) const;
 	Vector2& operator+=(const Vector2& other);
 	Vector2 operator-(const Vector2& other) const;
 	Vector2& operator-=(const Vector2& other);
 	bool operator==(const Vector2& other) const;
+	Vector2 operator*(float f) const;
+	Vector2& operator*=(float f);
+	Vector2 operator/(float f) const;
+	Vector2& operator/=(float f);
 };
--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,7 +1,16 @@
 #include "Vector2.h"
+#include <cmath>
 
 Vector2::Vector2(float x, float y)
 	: X(x), Y(y) {
+}
+
+Vector2 Vector2::Normalize() const {
+	float magnitude = std::sqrtf((this->X * this->X) + (this->Y * this->Y));
+	if (magnitude == 0.0f) {
+		return *this;
+	}
+	return *this / magnitude;
 }
 
 Vector2 Vector2::operator+(const Vector2& other) const {
@@ -26,4 +35,24 @@ Vector2& Vector2::operator-=(const Vector2& other) {
 
 bool Vector2::operator==(const Vector2& other) const {
 	return (this->X == other.X) && (this->Y == other.Y);
+}
+
+Vector2 Vector2::operator*(float f) const {
+	return Vector2(this->X * f, this->Y * f);
+}
+
+Vector2& Vector2::operator*=(float f) {
+	this->X *= f;
+	this->Y *= f;
+	return *this;
+}
+
+Vector2 Vector2::operator/(float f) const {
+	return Vector2(this->X / f, this->Y / f);
+}
+
+Vector2& Vector2::operator/=(float f) {
+	this->X /= f;
+	this->Y /= f;
+	return *this;
 }
